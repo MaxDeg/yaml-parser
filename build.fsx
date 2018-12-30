3a02fd2defd03ba3f46f678af1ebb3aec8342ea8
Maxime Degallaix <mdegallaix@localhost.localdomain>
2018-12-30 17:47:19 +0100

    some clean up

--- build.fsx
+++ build.fsx
@@ -25,29 +25,31 @@ let appReferences = !! "./**/*.fsproj"
 // -----------------------------------------------------------------------------
 // -- Targets
 // -----------------------------------------------------------------------------
-Target.create "Clean" <| fun _ ->
-    Shell.cleanDir buildDir
+Target.create "Clean" <| fun _ -> Shell.cleanDir buildDir
 
 Target.create "Restore" <| fun _ ->
-    appReferences
-    |> Seq.iter (fun p ->
-        DotNet.restore (fun args -> { args with NoCache = true }) p
-    )
+  let configureRestore (args : DotNet.RestoreOptions) =
+    { args with NoCache = true }
+
+  appReferences
+  |> Seq.iter (DotNet.restore configureRestore)
 
 Target.create "Build" <| fun _ ->
-    appReferences
-    |> Seq.iter (fun p ->
-        DotNet.build (fun args -> 
-            { args with Common =
-                { args.Common with CustomParams = Some "--no-restore" }
-            }) p
-    )
+  let configureBuild (args : DotNet.BuildOptions) =
+    { args with
+        Common = { args.Common with
+                    CustomParams = Some "--no-restore" } }
+
+  appReferences
+  |> Seq.iter (DotNet.build configureBuild)
 
 Target.create "Tests" <| fun _ ->
-    appReferences
-    |> Seq.filter (fun p -> p.Contains "tests")
-    |> Seq.iter (fun p -> 
-        DotNet.test (fun args -> { args with NoRestore = true }) p |> ignore)
+  let configureTest (args : DotNet.TestOptions) =
+    { args with NoRestore = true }
+
+  appReferences
+  |> Seq.filter (fun p -> p.Contains "tests")
+  |> Seq.iter (fun p -> DotNet.test configureTest p |> ignore)
 
 // -----------------------------------------------------------------------------
 // -- Dependencies
